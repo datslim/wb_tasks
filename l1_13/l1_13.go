033b291299474a54ad09db21d8af0fe83d7ef531
lucoscly <lucoscly@student.21-school.ru>
2025-07-08 22:34:48 +0300

    feat: [l1_13]

---
+++ l1_13/l1_13.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// функция для замены значений двух чисел без временной переменной используя сложение\вычитание
+// функция принимает указатель на два числа, которые нужно поменять между собой
+// a = 5, b = 9
+// a = 5 + 9 = 14
+// b = 14 - 9 = 5
+// a = 14 - 5 = 9
+// a = 9, b = 5
+func changeTwoInts(first, second *int) {
+	*first += *second
+	*second = *first - *second
+	*first -= *second
+
+}
+
+func main() {
+	first, second := 5, 9
+	fmt.Printf("Перед заменой\nПервое значение: %d, второе значение: %d\n", first, second)
+	changeTwoInts(&first, &second)
+	fmt.Printf("После замены\nПервое значение: %d, второе значение: %d\n", first, second)
+}
