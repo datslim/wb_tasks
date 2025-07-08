5728f5eaabfa4673a095439553b5a612802aa5aa
lucoscly <lucoscly@student.21-school.ru>
2025-07-08 20:52:42 +0300

    rename: [l1_19] stringRunes -> inputRunes

--- l1_19/l1_19.go
+++ l1_19/l1_19.go
@@ -6,19 +6,19 @@ import "fmt"
 func reverseString(inputString string) string {
 	// для правильной обработки символов, размер которых не равен 1 байту и нам не известен, приведем входную строку
 	// к типу []rune и будем менять порядок символов in-place
-	stringRunes := []rune(inputString)
+	inputRunes := []rune(inputString)
 
 	// используем два указателя, один из которых указывает на начало, а второй на конец исходного среза
 	// таким образом за один проход, мы можем переписать наш слайс на обратный порядок символов
 	l := 0
-	r := len(stringRunes) - 1
+	r := len(inputRunes) - 1
 
 	for l <= r { // пока указатели не встретились
-		stringRunes[l], stringRunes[r] = stringRunes[r], stringRunes[l] // меняем начало с концом
-		l++                                                             // левый указатель двигаем вперед
-		r--                                                             // правый указатель двигаем назад
+		inputRunes[l], inputRunes[r] = inputRunes[r], inputRunes[l] // меняем начало с концом
+		l++                                                         // левый указатель двигаем вперед
+		r--                                                         // правый указатель двигаем назад
 	}
-	return string(stringRunes) // приводим наш срез к строке
+	return string(inputRunes) // приводим наш срез к строке
 }
 
 func main() {
