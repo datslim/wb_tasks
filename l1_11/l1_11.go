50156826244bdcf03d8c88345eba6261216e857b
lucoscly <lucoscly@student.21-school.ru>
2025-07-10 21:39:02 +0300

    fix: [l1_11] use test funcs from miscs

--- l1_11/l1_11.go
+++ l1_11/l1_11.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"wb_tech/misc"
 )
 
 // функция для нахождения пересечения двух неупорядочных множеств (слайсов), содержащих любые данные
@@ -32,42 +32,17 @@ func setsIntersection(firstSet, secondSet []any) []any {
 	return intersection
 }
 
-// функция для заполнения слайса случайными числами
-func fillSetWithRandomNumbers(set []any) {
-	for i := range set {
-		randomValue := rand.Int() % 10
-		set[i] = randomValue
-	}
-}
-
-// функция для заполнения слайса случайными значениями
-func fillSetWithRandomValues(set []any) {
-	// возможные значения для записи
-	possibleValues := []any{
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-		"apple", "banana", "orange", "l1", "webdev", "wb-tech", "cat", "dog",
-		1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9,
-		true, false,
-		nil, "golang", "programming", 42, 3.1415,
-	}
-
-	for i := range set {
-		randomIndex := rand.Intn(len(possibleValues))
-		set[i] = possibleValues[randomIndex]
-	}
-}
-
 func main() {
 	firstSet := make([]any, 5) // создаем множество размером 5
 	secondSet := make([]any, 5)
-	fillSetWithRandomNumbers(firstSet) // заполняем случайными числами
-	fillSetWithRandomNumbers(secondSet)
+	misc.FillAnySetWithRandomNumbers(firstSet) // заполняем случайными числами
+	misc.FillAnySetWithRandomNumbers(secondSet)
 	fmt.Printf("Первое множество чисел: %v\nВторое множество чисел: %v\nПересечение множеств чисел: %v\n\n", firstSet, secondSet, setsIntersection(firstSet, secondSet))
 
 	anySet1 := make([]any, 10) // создаем множество размером 10
 	anySet2 := make([]any, 10)
 
-	fillSetWithRandomValues(anySet1) // заполняем случайными значениями
-	fillSetWithRandomValues(anySet2)
+	misc.FillSetWithRandomValues(anySet1) // заполняем случайными значениями
+	misc.FillSetWithRandomValues(anySet2)
 	fmt.Printf("Первое множество: %v\nВторое множество: %v\nПересечение множеств: %v\n", anySet1, anySet2, setsIntersection(anySet1, anySet2))
 }
