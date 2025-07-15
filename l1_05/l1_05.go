4358cba5dce71cffa9c058d16756e499a4e8bc14
lucoscly <lucoscly@student.21-school.ru>
2025-07-15 22:29:06 +0300

    feat: [l1_05]

---
+++ l1_05/l1_05.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// функция для постоянной записи в канал (пока не закроется контекст)
+// аргументы: контекст, группа ожидания, канал
+func writer(ctx context.Context, wg *sync.WaitGroup, data chan int) {
+	wg.Add(1) // добавляем 1 в вейтгруппу
+
+	go func() { // запускаем горутину
+		defer wg.Done() // уменьшаем счетчик группы ожидания
+		i := 0          // счетчик
+		for {
+			select {
+			case data <- i: // записываем в канал
+				i++ // увеличиваем счетчик
+			case <-ctx.Done(): // если контекст завершен, закрываем канал и выходим из функции
+				close(data) //
+				return
+			}
+		}
+	}()
+}
+
+// функция для чтения из канала (пока не закроется канал)
+// аргументы: группа ожидания, канал
+func reader(wg *sync.WaitGroup, data chan int) {
+	wg.Add(1) // добавляем 1 в вейтгруппу
+
+	go func() { // запускаем горутину
+		defer wg.Done()       // уменьшаем счетчик группы ожидания
+		for i := range data { // читаем из канала
+			fmt.Printf("Значение: %d\n", i)
+		}
+		fmt.Println("Канал закрыт.")
+	}()
+}
+
+func main() {
+	fmt.Printf("Введите количество секунд: ")
+	var N int
+	fmt.Scan(&N) // считываем количество секунд
+
+	data := make(chan int) // создаем канал для передачи данных
+
+	// создаем контекст с таймаутом в N секунд и отменяем его по завершении
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(N)*time.Second)
+	defer cancel() // отменяем контекст по завершении
+
+	var wg sync.WaitGroup  // создаем группу ожидания
+	writer(ctx, &wg, data) // запускаем функцию для записи в канал
+	reader(&wg, data)      // запускаем функцию для чтения из канала
+
+	wg.Wait() // ожидаем завершения всех горутин
+
+	fmt.Printf("Завершено через %d секунд(ы)\n", N)
+}
